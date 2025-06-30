6d71491cda3c30fe5a1fcb1e2de7f81c8683d92b
10Fawaz <faaljar2@gmail.com>
2025-06-30 15:03:08 +0300

    I have added 5 "car" Category Questions and answers..

---
+++ script.js
@@ -0,0 +1,39 @@
+const myCategories = {
+  cars: [
+    {
+      question: 'Which car brand has a prancing horse as its logo?',
+      options: ['Ferrari', 'Porche', 'Lamborghini', 'BMW'],
+      answer: ['Ferrari']
+    },
+    {
+      question: 'What country is home. to the car manufacturer Toyota?',
+      options: ['China', 'Japan', 'South Korea', 'Germany'],
+      answer: ['Japan']
+    },
+    {
+      question: 'Who founded the Ford Motor company?',
+      options: ['Enzo Ferrari', 'Henry Ford', 'Karl Benz', 'Elon Musk'],
+      answer: ['Henry Ford']
+    },
+    {
+      question: 'What does "EV" stand for in the automotive world?',
+      options: [
+        'Engine Vehicle',
+        'Electric Vehicle',
+        'Emission Valve',
+        'Eco Van'
+      ],
+      answer: ['Electric vehicle']
+    },
+    {
+      question: 'Which of these is a typle of a drivetrain.',
+      options: [
+        'Front-Wheel Drive',
+        'All-Screen Display',
+        'Head-Up Display',
+        'Turbo Intake'
+      ],
+      answer: ['Front-Wheel Drive']
+    }
+  ]
+}
